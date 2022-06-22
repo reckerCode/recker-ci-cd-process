f4e17c1e6030d282965658eb9ea26cb75e22953e
Hardik Gunjal <hardikgunjal@Hardiks-MacBook-Pro.local>
2022-06-22 11:41:30 -0400

    inital commit

---
+++ src/main/java/re/cicd/example/CICDExample/CicdExampleApplication.java
@@ -0,0 +1,13 @@
+package re.cicd.example.CICDExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/re/cicd/example/CICDExample/CicdExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package re.cicd.example.CICDExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
